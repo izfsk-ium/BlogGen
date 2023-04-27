2017b21459511beefa38e0727910bd90775339aa
izfsk <118359166+izfsk-ium@users.noreply.github.com>
2023-04-27 18:55:12 +0800

    Update metadata.py

--- lib/metadata.py
+++ lib/metadata.py
@@ -5,11 +5,12 @@ The article metadata
 """
 
 import uuid
-
+import re
 from enum import Enum
 from datetime import datetime
 import os
 import toml
+from msgpack import packb
 
 from .blog_utils import BlogUtils
 
@@ -52,7 +53,20 @@ class ArticleMetaData:
         )
 
 
+def remove_markdown(text):
+    text = re.sub(r"^#*", "", text, flags=re.MULTILINE)
+    text = re.sub(r"(\*\*|__)(.*?)\1", r"\2", text)
+    text = re.sub(r"(\*|_)(.*?)\1", r"\2", text)
+    text = re.sub(r"\[(.*?)\]\(.*?\)", r"\1", text)
+    text = re.sub(r"!\[(.*?)\]\(.*?\)", r"\1", text)
+    text = re.sub(r"`(.+?)`", r"\1", text)
+    text = re.sub(r"```(.+?)```", "", text, flags=re.DOTALL)
+    return text
+
+
 class BlogArticles(object):
+    isGenerated: bool = False
+
     def __new__(cls):
         if not hasattr(cls, "instance"):
             cls.instance = super(BlogArticles, cls).__new__(cls)
@@ -69,6 +83,28 @@ class BlogArticles(object):
                     markdown_file_path, toml.load(open(configure_file_path, "r"))
                 )
             )
+        self.generateAllArticleSearchData()
+
+    def generateAllArticleSearchData(self):
+        if not BlogArticles.isGenerated:
+            searchData = []
+            for i in [i for i in self.all_articles]:
+                _, content = open(i.filePath).read().split("---", 2)[1:]
+                searchData.append(
+                    {
+                        "title": i.title,
+                        "path": f"/articles/{i.title}/index.html",
+                        "date": i.createDate.timestamp(),
+                        "data": remove_markdown(
+                            "".join([i.strip() for i in content.split()])
+                        ),
+                    }
+                )
+            BlogArticles.isGenerated = True
+            with open("output/overview.msgpack", "wb") as fp:
+                fp.write(packb(searchData, use_bin_type=True))
+                fp.flush()
+                fp.close()
 
     def getAllArticleMetadata(self) -> list[ArticleMetaData]:
         return self.all_articles
